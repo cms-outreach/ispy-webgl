57de3c4fcaa878843cee506fca193e3549d6fe62
tpmccauley <tpmccauley@gmail.com>
2014-11-21 19:56:54 +0000

    add tracker models

--- js/display.js
+++ js/display.js
@@ -133,6 +133,99 @@ yae.addGroups = function() {
   });
 }
 
+yae.makeDisc = function(ir, or, pos, slices) {
+  var lines = [];
+  var points = [];
+
+  for (var i = 0; i < slices; i++) {
+    var sa = Math.sin(i / slices * 2 * Math.PI);
+    var ca = Math.cos(i / slices * 2 * Math.PI);
+
+    points.push({x: ir * sa, y: ir * ca, z: pos});
+    points.push({x: or * sa, y: or * ca, z: pos});
+
+    var ix1 = i * 2;
+    var ix2 = ((i + 1) % slices) * 2;
+
+    lines.push({p1: ix1 + 0, p2: ix1 + 1});
+    lines.push({p1: ix1 + 0, p2: ix2 + 0});
+    lines.push({p1: ix1 + 1, p2: ix2 + 1});
+  }
+
+  return [points, lines];
+}
+
+yae.makeCylinder = function(r, len, pos, slices, segments) {
+  var lines = [];
+  var points = [];
+  for (var s = 0; s < segments; s++) {
+    for (var i = 0; i < slices; i++) {
+      var sa = Math.sin(i / slices * 2 * Math.PI);
+      var ca = Math.cos(i / slices * 2 * Math.PI);
+      var p = s/(segments - 1) * len + pos;
+      points.push({x: r * sa, y: r * ca, z: p});
+
+      var ix1 = (s * slices + i);
+      var ix2 = (s * slices + ((i + 1) % slices));
+
+      lines.push({p1: ix1, p2: ix2});
+
+      if (s > 0) {
+        var ix3 = ((s - 1) * slices + i);
+        lines.push({p1: ix3 + 0, p2: ix1 + 0});
+      }
+    }
+  }
+
+  return [points, lines];
+}
+
+yae.makeModelTrackerBarrel = function() {
+  var radii = [0.046, 0.07, 0.1,
+               0.24, 0.27, 0.32, 0.37, 0.40, 0.43, 0.47, 0.51,
+               0.62, 0.70, 0.78, 0.88, 0.97, 1.08];
+  var lengths = [0.53, 0.53, 0.53,
+                 1.3, 1.3, 1.3, 1.3, 1.3, 1.3, 1.3, 1.3,
+                 2.18, 2.18, 2.18, 2.18, 2.18, 2.18];
+
+  var slices = 24;
+  var wfs = new Array();
+
+  for (var i = 0; i < radii.length; i++) {
+    var r = radii[i];
+    var l = lengths[i];
+
+    wfs.push(yae.makeCylinder(r, l, -l / 2, slices, 2));
+  }
+
+  return wfs;
+}
+
+yae.makeModelTrackerEndcap = function() {
+  var ecradii =    [0.145, 0.145, 0.50, 0.47, 0.50, 0.47, 0.50, 0.47,
+                    1.08, 1.06, 1.08, 1.06, 1.08, 1.06, 1.08, 1.06, 1.08, 1.06, 1.08, 1.06,
+                    1.08, 1.06, 1.08, 1.06, 1.08, 1.06];
+  var ecintradii = [0.07,  0.07,  0.40, 0.25, 0.40, 0.25, 0.40, 0.25,
+                    0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30,
+                    0.30, 0.30, 0.30, 0.30, 0.30, 0.30];
+  var ecpos =      [0.35,  0.48,  0.76, 0.83, 0.89, 0.96, 1.02, 1.09,
+                    1.27, 1.34, 1.41, 1.48, 1.55, 1.62, 1.69, 1.76, 1.83, 1.90, 2.00, 2.08,
+                    2.20, 2.28, 2.40, 2.48, 2.60, 2.68];
+
+  var slices = 24;
+  var wfs = new Array();
+
+  for (var i = 0; i < ecradii.length; i++) {
+    var ecro = ecradii[i];
+    var ecri = ecintradii[i];
+    var ecp = ecpos[i];
+
+    wfs.push(yae.makeDisc(ecri, ecro, ecp, slices));
+    wfs.push(yae.makeDisc(ecri, ecro, -ecp, slices));
+  }
+  return wfs;
+}
+
 yae.makeWireframeBox = function(data, style, ci) {
 
   var f1 = new THREE.Vector3(data[ci][0],   data[ci][1],   data[ci][2]);
@@ -192,6 +285,75 @@ yae.makeWireframeBox = function(data, style, ci) {
   return box;
 }
 
+yae.makeShapes = function(data, style) {
+  points = data[0];
+  lines = data[1];
+  shapes = [];
+
+  var wfcolor = new THREE.Color();
+    wfcolor.setRGB(style.color[0], style.color[1], style.color[2]);
+  var material = new THREE.LineBasicMaterial({color:wfcolor, linewidth:style.linewidth, opacity: style.opacity});
+
+  for ( var i = 0; i < lines.length; i++ ) {
+    var l = lines[i];
+    var line = new THREE.Geometry();
+    var p1 = points[l.p1];
+    var p2 = points[l.p2];
+    line.vertices.push(points[l.p1]);
+    line.vertices.push(points[l.p2]);
+    shapes.push(new THREE.Line(line,material));
+  }
+
+  return shapes;
+}
+
+yae.makeModelTrackerBarrel = function() {
+  var radii = [0.046, 0.07, 0.1,
+               0.24, 0.27, 0.32, 0.37, 0.40, 0.43, 0.47, 0.51,
+               0.62, 0.70, 0.78, 0.88, 0.97, 1.08];
+  var lengths = [0.53, 0.53, 0.53,
+                 1.3, 1.3, 1.3, 1.3, 1.3, 1.3, 1.3, 1.3,
+                 2.18, 2.18, 2.18, 2.18, 2.18, 2.18];
+
+  var slices = 24;
+  var wfs = new Array();
+
+  for (var i = 0; i < radii.length; i++) {
+    var r = radii[i];
+    var l = lengths[i];
+
+    wfs.push(yae.makeCylinder(r, l, -l / 2, slices, 2));
+  }
+
+  return wfs;
+}
+
+yae.makeModelTrackerEndcap = function() {
+  var ecradii =    [0.145, 0.145, 0.50, 0.47, 0.50, 0.47, 0.50, 0.47,
+                    1.08, 1.06, 1.08, 1.06, 1.08, 1.06, 1.08, 1.06, 1.08, 1.06, 1.08, 1.06,
+                    1.08, 1.06, 1.08, 1.06, 1.08, 1.06];
+  var ecintradii = [0.07,  0.07,  0.40, 0.25, 0.40, 0.25, 0.40, 0.25,
+                    0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30, 0.30,
+                    0.30, 0.30, 0.30, 0.30, 0.30, 0.30];
+  var ecpos =      [0.35,  0.48,  0.76, 0.83, 0.89, 0.96, 1.02, 1.09,
+                    1.27, 1.34, 1.41, 1.48, 1.55, 1.62, 1.69, 1.76, 1.83, 1.90, 2.00, 2.08,
+                    2.20, 2.28, 2.40, 2.48, 2.60, 2.68];
+
+  var slices = 24;
+  var wfs = new Array();
+
+  for (var i = 0; i < ecradii.length; i++) {
+    var ecro = ecradii[i];
+    var ecri = ecintradii[i];
+    var ecp = ecpos[i];
+
+    wfs.push(yae.makeDisc(ecri, ecro, ecp, slices));
+    wfs.push(yae.makeDisc(ecri, ecro, -ecp, slices));
+  }
+  return wfs;
+}
+
+
 yae.makeDT = function(dt, style) {
   return yae.makeWireframeBox(dt, style, 1);
 }
@@ -204,13 +366,36 @@ yae.POINT = 0;
 yae.LINE = 1;
 yae.BOX = 2;
 yae.SCALEDBOX = 3;
-yae.TRACK = 4;
+yae.MODEL = 4;
+yae.TRACK = 5;
 
 yae.data_description = {
+  "TrackerBarrel3D_MODEL": {type: yae.MODEL, on: false, group: "Detector", name: "Tracker Barrels",
+    fn: yae.makeModelTrackerBarrel, style: {color: [1, 1, 0], opacity: 0.3, linewidth: 1.0}},
+  "TrackerEndcap3D_MODEL": {type: yae.MODEL, on: false, group: "Detector", name: "Tracker Endcaps",
+    fn: yae.makeModelTrackerEndcap, style: {color: [1, 1, 0], opacity: 0.3, linewidth: 0.5}},
+  /*
+  "EcalBarrel3D_MODEL": {type: yae.MODEL, on: true, group: "Detector", desc: "ECAL Barrel",
+    fn: makeModelEcalBarrel, style: {color: [0, 1, 1], opacity: 0.5, linewidth: 0.5}},
+  "EcalEndcap3D_plus": {type: yae.MODEL, on: false, group: "Detector", desc: "ECAL Endcap (+)",
+    fn: makeModelEcalEndcapPlus, style: {color: [0, 1, 1], opacity: 0.5, linewidth: 0.5}},
+  "EcalEndcap3D_minus": {type: yae.MODEL, on: false, group: "Detector", desc: "ECAL Endcap (-)",
+    fn: makeModelEcalEndcapMinus, style: {color: [0, 1, 1], opacity: 0.5, linewidth: 0.5}},
+  "HcalBarrel3D_MODEL": {type: yae.MODEL, on: false, group: "Detector", desc: "HCAL Barrel",
+    fn: makeModelHcalBarrel, style: {color: [0.8, 1, 0], opacity: 0.5, linewidth: 0.5}},
+  "HcalEndcap3D_MODEL": {type: yae.MODEL, on: false, group: "Detector", desc: "HCAL Endcaps",
+    fn: makeModelHcalEndcap, style: {color: [0.8, 1, 0], opacity: 0.5, linewidth: 0.5}},
+  "HcalOuter3D_MODEL": {type: yae.MODEL, on: true, group: "Detector", desc: "HCAL Outer",
+    fn: makeModelHcalOuter, style: {color: [0.8, 1, 0], opacity: 0.5, linewidth: 0.5}},
+  "HcalForward3D_plus": {type: yae.MODEL, on: false, group: "Detector", desc: "HCAL Forward (+)",
+    fn: makeModelHcalForwardPlus, style: {color: [0.8, 1, 0], opacity: 0.5, linewidth: 0.5}},
+  "HcalForward3D_minus": {type: yae.MODEL, on: false, group: "Detector", desc: "HCAL Forward (-)",
+    fn: makeModelHcalForwardMinus, style: {color: [0.8, 1, 0], opacity: 0.5, linewidth: 0.5}},
+  */
   "DTs3D_V1": {type: yae.BOX, on: false, group: "Detector", name: "Drift Tubes (muon)",
     fn: yae.makeDT, style: {color: [1, 0.6, 0], opacity: 0.3, linewidth: 0.9}},
   "CSC3D_V1": {type: yae.BOX, on: false, group: "Detector", name: "Cathode Strip Chambers (muon)",
-    fn: yae.makeCSC, style: {color: [0.6, 0.7, 0], opacity: 0.3, linewidth: 0.8}},
+    fn: yae.makeCSC, style: {color: [0.6, 0.7, 0], opacity: 0.3, linewidth: 0.8}}
 };
 
 yae.disabled = new Array();
@@ -275,6 +460,19 @@ yae.addDetector = function() {
           }
         break;
 
+        case yae.MODEL:
+          var models = descr.fn();
+
+          for ( var j = 0; j < models.length; j++ ) {
+            var shapes = yae.makeShapes(models[j], descr.style);
+
+            shapes.forEach(function(s) {
+              s.name = key;
+              s.visible = visible;
+              yae.scene.getObjectByName(descr.group).add(s);
+            });
+          }
+        break;
       }
   }
 }
