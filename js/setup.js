3bb6bb1f3a3ea8501621e6bc69ad0bf5f76f41d0
tpmccauley <tpmccauley@gmail.com>
2014-12-15 16:07:15 +0000

    rm canavs test condition

--- js/setup.js
+++ js/setup.js
@@ -3,7 +3,6 @@ ispy.detector = {"Collections":{}};
 ispy.version = "0.0.1";
 
 ispy.hasWebGL = function() {
-  return false;
   var canvas = document.createElement('canvas');
 
   if ( ! canvas.getContext('webgl') ) {
