9905ca6ec1e5471a7b1506cc59b2f37d6e798e96
tpmccauley <tpmccauley@gmail.com>
2015-01-26 15:07:06 +0000

    add progress bar for web file loading

--- js/load.js
+++ js/load.js
@@ -3,7 +3,8 @@
 ispy.web_files = [
   "./data/4lepton.ig",
   "./data/Mu.ig",
-  "./data/Electron.ig"
+  "./data/Electron.ig",
+  "./data/masterclass_1.ig"
 ];
 
 ispy.ig_data = null;
@@ -70,7 +71,7 @@ ispy.enableNextPrev = function() {
 
 ispy.loadEvent = function() {
   $("#event-loaded").html("");
-  $("#progress").modal("show");
+  $("#loading").modal("show");
 
   try {
     var event = JSON.parse(ispy.cleanupData(ispy.ig_data.file(ispy.event_list[ispy.event_index]).asText()));
@@ -78,7 +79,7 @@ ispy.loadEvent = function() {
     alert(err);
   }
 
-  $("#progress").modal("hide");
+  $("#loading").modal("hide");
   ispy.addEvent(event);
   ispy.enableNextPrev();
 
@@ -161,6 +162,8 @@ ispy.loadLocalFiles = function() {
 ispy.selectFile = function(filename) {
   ispy.file_name = filename.split("/")[2];  // of course this isn't a general case for files
 
+  $('#progress').modal('show');
+
   var xhr = new XMLHttpRequest();
   xhr.open("GET", filename, true);
   xhr.overrideMimeType("text/plain; charset=x-user-defined");
@@ -169,8 +172,22 @@ ispy.selectFile = function(filename) {
   var ecell = document.getElementById("browser-events").insertRow(0).insertCell(0);
   ecell.innerHTML = 'Loading events...';
 
+  xhr.onprogress = function(evt) {
+    if ( evt.lengthComputable ) {
+     var percentComplete = Math.round((evt.loaded / evt.total)*100);
+     $('.progress-bar').attr('style', 'width:'+percentComplete+'%;');
+     $('.progress-bar').html(percentComplete+'%');
+   }
+  }
+
+  xhr.onreadystatechange = function () {
+    if (this.readyState === 4){
+      $('#progress').modal('hide');
+    }
+  }
+
   xhr.onload = function() {
-    if (this.status == 200) {
+    if (this.status === 200) {
 
       var zip = JSZip(xhr.responseText);
       var event_list = [];
