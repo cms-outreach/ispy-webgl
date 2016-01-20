be1314afa2c4d425ae521c508a2cccbf450c357a
tpmccauley <tpmccauley@gmail.com>
2016-01-20 11:01:19 +0000

    add ref to samples file

--- js/files-load.js
+++ js/files-load.js
@@ -1,6 +1,7 @@
 // For now, hard-code some examples files here for testing
 // the "files from the web"
 ispy.web_files = [
+  "./masterclass-2015/masterclass_samples.ig",
   "./masterclass-2015/masterclass_1.ig",
   "./masterclass-2015/masterclass_2.ig",
   "./masterclass-2015/masterclass_3.ig",
