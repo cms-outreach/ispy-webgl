e285218c7edc55b73ee7306dec07de28ae79c4ce
tpmccauley <tpmccauley@gmail.com>
2016-02-16 15:55:21 +0000

    increment version

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 var ispy = ispy || {};
 ispy.detector = {"Collections":{}};
-ispy.version = "0.9.4-invenio.17";
+ispy.version = "0.9.4-invenio.18";
 
 // These need to be defined before adding objects:
 ispy.POINT = 0;
