a42925b7a86b52c2b540f1aa2b5121dc1dff4fc8
Tom McCauley <tpmccauley@gmail.com>
2022-07-01 17:34:03 +0200

    all together now

--- js/setup.js
+++ js/setup.js
@@ -172,7 +172,69 @@ ispy.init = function() {
     ispy.clipgui.domElement.id = 'clipgui';
     document.getElementById('titlebar').appendChild(ispy.clipgui.domElement);
 
+    const planeX = ispy.clipgui.addFolder("planeX");
+    const planeY = ispy.clipgui.addFolder("planeY");
+    const planeZ = ispy.clipgui.addFolder("planeZ");
+
+    const clip_params = {
+    
+	planeX: {
+	    constant: 10,
+	    negated: false 
+	},
+    
+	planeY: {
+	    constant: 10,
+	    negated: false
+	},
+    
+	planeZ: {
+	    constant: 30,
+	    negated: false
+	}
+	
+    };
+
+    ispy.planes = [
+	new THREE.Plane( new THREE.Vector3(-1,0,0), clip_params.planeX.constant),
+	new THREE.Plane( new THREE.Vector3(0,-1,0), clip_params.planeY.constant),
+	new THREE.Plane( new THREE.Vector3(0,0,-1), clip_params.planeZ.constant)
+    ];
+    
     ispy.renderer.clippingPlanes = ispy.planes;
+    
+    planeX.add(clip_params.planeX, 'constant').min(-10).max(10).onChange(
+	d => ispy.planes[0].constant = d
+    );
+
+    planeX.add(clip_params.planeX, 'negated').onChange(() => {
+	ispy.planes[0].negate();
+	clip_params.planeX.constant = ispy.planes[0].constant;
+    });
+
+    planeX.open();
+
+    planeY.add(clip_params.planeY, 'constant').min(-10).max(10).onChange(
+	d => ispy.planes[1].constant = d
+    );
+
+    planeY.add(clip_params.planeY, 'negated').onChange(() => {
+	ispy.planes[1].negate();
+	clip_params.planeY.constant = ispy.planes[1].constant;
+    });
+
+    planeY.open();
+
+    planeZ.add(clip_params.planeZ, 'constant').min(-30).max(30).onChange(
+	d => ispy.planes[2].constant = d
+    );
+
+    planeZ.add(clip_params.planeZ, 'negated').onChange(() => {
+	ispy.planes[2].negate();
+	clip_params.planeZ.constant = ispy.planes[2].constant;
+    });
+
+    planeZ.open();
 
     ispy.inverted_colors = false;
     $('#invert-colors').prop('checked', false);
